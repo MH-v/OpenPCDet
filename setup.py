76c7fe531ff4811976293edb0a033d4ebbea8e76
MH-v <m.h.vd.velde@proton.me>
2025-12-02 14:28:35 +0100

    Fixed version mismatches

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
         install_requires=[
             'numpy',
             'llvmlite',
-            'numba',
+            'numba-cuda',
             'tensorboardX',
             'easydict',
             'pyyaml',
